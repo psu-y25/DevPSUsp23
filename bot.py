a8826ac43e367c81ce7eb2e11ff881659e29d331
yqa5116 <yqa5116@psu.edu>
2023-02-14 19:44:04 -0500

    bot can now react to certain messages

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   #?
 token = os.getenv('TOKEN')
 
 intents = discord.Intents.default()
@@ -10,11 +10,28 @@ intents.message_content = True
 
 
 class DevPSUBot(discord.Client):
-    pass
-    
+    async def on_ready(self):       #async does ?
+        print("logged on!")
+    async def on_message(self, message):
+        print(f"message found: {message.content} from {message.author} in {message.channel}")
+        if message.author == client.user:
+            return
+
+        if message.content.lower() == "hello": #when someone says hello
+            await message.channel.send("Hello!")    #bot sends this message 
+
+        if client.user in message.mentions:
+            print("bot mentioned!")
+            await message.channel.send("I was mentioned") #bot sends this message
+        
+        if message.content == "react":
+            print("react command recognized")
+            await message.add_reaction("👍") #you can add another line so that the bot can do more than one reaction
+
+    async def on_typing(self, channel, user, when):
+        print(f"{user} is typing in {channel} at {when}")
+        await channel.send(f"I see you typing, {user}")
 
 
-
-client = discord.Client(intents=intents)
+client = DevPSUBot(intents=intents) #difference betweent intents and intents=intents?
 client.run(token)
-#testing
